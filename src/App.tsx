5228c491f453ff71a4baf4e86d0e512594091f21
gabhpadilha02 <gabh.padilha2@gmail.com>
2022-08-17 13:02:08 -0300

    Adding react-router-dom and config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
@@ -7,13 +8,10 @@ import { GlobalStyle } from './styles/global'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
