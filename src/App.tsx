0f8e2b7f60cb69ab0572d2bee02aaa3c3c704a06
gabhpadilha02 <gabh.padilha2@gmail.com>
2022-08-19 13:35:32 -0300

    organizing contexts in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { CyclesContextProvider } from './context/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
-        <GlobalStyle />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
